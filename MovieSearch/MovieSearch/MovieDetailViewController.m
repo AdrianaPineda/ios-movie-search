08fb74750e5f5fb0320c07737f0ec4231060e297
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-03-13 18:59:41 -0500

    Add SMS share code

--- MovieSearch/MovieSearch/MovieDetailViewController.m
+++ MovieSearch/MovieSearch/MovieDetailViewController.m
@@ -8,6 +8,13 @@
 
 #import "MovieDetailViewController.h"
 #import "Social/Social.h"
+#import <AFNetworking/AFNetworking.h>
+
+#define TWILIO_SMS_URL @"https://api.twilio.com/2010-04-01/Accounts/%@/Messages.json"
+#define TWILIO_ACCOUNT_SID @"AC2bb2afd3df3290ba3354e96e8334e437"
+#define TWILIO_AUTH_TOKEN @"787cbcd2a34fa11896e3c64c779210e6"
+#define FROM_NUMBER @"+19253266669"
+#define DEFAULT_TO_NUMBER @"+573007888618"
 
 @interface MovieDetailViewController ()
 
@@ -68,6 +75,7 @@
 
     UIAlertAction *smsAction = [UIAlertAction actionWithTitle:@"SMS" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
 
+        [self sendSMS];
     }];
 
     [alertController addAction:twitterAction];
@@ -87,6 +95,33 @@
         [self presentViewController:twitterController animated:YES completion:nil];
     }
 }
+
+- (void)sendSMS {
+
+    NSString *mesage = [NSString stringWithFormat:@"I'm watching %@", self.movie.title];
+    NSString *urlAsString = [NSString stringWithFormat:TWILIO_SMS_URL, TWILIO_ACCOUNT_SID];
+    NSDictionary *paramsAsDict = @{@"To": DEFAULT_TO_NUMBER, @"From": FROM_NUMBER, @"Body": mesage};
+
+    NSURL *URL = [NSURL URLWithString:urlAsString];
+
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    [manager.requestSerializer setAuthorizationHeaderFieldWithUsername:TWILIO_ACCOUNT_SID password:TWILIO_AUTH_TOKEN];
+
+    [manager POST:URL.absoluteString parameters:paramsAsDict progress:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        NSError* error;
+        NSDictionary* json = [NSJSONSerialization JSONObjectWithData:responseObject
+                                                             options:kNilOptions
+                                                               error:&error];
+        NSString *messageContent = [json objectForKey:@"body"];
+        NSLog(@"%@",messageContent);
+
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        NSLog(@"Error: %@", error);
+
+    }];
+}
+
 /*
 #pragma mark - Navigation
 
