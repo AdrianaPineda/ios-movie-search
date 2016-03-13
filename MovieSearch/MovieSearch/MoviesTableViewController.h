b7a9b769718a5cffb5385897fb92a79eea7f77bc
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-03-13 14:29:52 -0500

    Add MoviesTableViewController class and link it with view on storyboard

---
+++ MovieSearch/MovieSearch/MoviesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoviesTableViewController.h
+//  MovieSearch
+//
+//  Created by Adriana Pineda on 3/13/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoviesTableViewController : UITableViewController
+
+@end
