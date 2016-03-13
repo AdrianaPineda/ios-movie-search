f2084767a4fdc8b5525fa61cd82528b992aa81a2
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-03-13 16:54:17 -0500

    Add initialization method in Movie class

--- MovieSearch/MovieSearch/Models/Movie.m
+++ MovieSearch/MovieSearch/Models/Movie.m
@@ -8,6 +8,24 @@
 
 #import "Movie.h"
 
+#define YEAR_KEY @"Year"
+#define TITLE_KEY @"Title"
+#define POSTER_URL_KEY @"Poster"
+#define IMDB_ID_KEY @"Poster"
+
 @implementation Movie
 
+- (instancetype) initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+
+    if (self) {
+
+        self.year = [[dictionary objectForKey:YEAR_KEY] integerValue];
+        self.title = [dictionary objectForKey:TITLE_KEY];
+        self.posterUrl = [dictionary objectForKey:POSTER_URL_KEY];
+        self.imdbID = [dictionary objectForKey:IMDB_ID_KEY];
+    }
+
+    return self;
+}
 @end
