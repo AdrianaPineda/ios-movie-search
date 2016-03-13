9df7593200ad81da1e79a1571a473f44783f68db
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-03-13 14:28:42 -0500

    Add Movie class to project. This class will include info about a movie

---
+++ MovieSearch/MovieSearch/Models/Movie.h
@@ -0,0 +1,13 @@
+//
+//  Movie.h
+//  MovieSearch
+//
+//  Created by Adriana Pineda on 3/13/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Movie : NSObject
+
+@end
