1faba26e3c93aebc53204d8edc359f9c4432d9fd
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-03-13 14:06:47 -0500

    Create Xcode project

---
+++ MovieSearch/MovieSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieSearch
+//
+//  Created by Adriana Pineda on 3/13/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MovieSearch/MovieSearch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MovieSearch
+//
+//  Created by Adriana Pineda on 3/13/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
